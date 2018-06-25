0fe5cb3eb9c47888b3e2ff0eceb1fbeddc0c9532
November Williams <nnwilliams8@gmail.com>
2018-06-25 11:52:35 -0400

    generic commit message--command was run by script

---
+++ wrap.js
@@ -0,0 +1,18 @@
+const wrap = (line, maxLen) => {
+  if (typeof line !== 'string' || typeof maxLen !== 'number') {
+    throw new TypeError(
+      'inputs must be of type string and type number respectively'
+    );
+  }
+
+  if (maxLen === 0 || maxLen > line.length) return line;
+  let wrapped = line;
+  for (let i = maxLen; i < Math.ceil(line.length / maxLen); i += maxLen) {
+    let breakSpace = line.lastIndexOf(' ', i);
+    wrapped =
+      wrapped.slice(0, breakSpace) + '\n' + wrapped.slice(breakSpace + 1);
+  }
+  return wrapped;
+};
+
+module.exports = wrap;
---
+++ wrap.spec.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+const wrap = require('./wrap');
+
+describe('wrap', () => {
+  it('Returns empty string if empty string was provided', () => {
+    expect(wrap('', 10)).to.equal('');
+  });
+  it('throws an error if the inputs are of wrong type', () => {
+    expect(() => wrap(13, 'hello')).to.throw();
+  });
+  it("does not alter original input if specified line break is greater than that input's length", () => {
+    expect(wrap('O valiant cousin! worthy gentleman!', 100)).to.equal(
+      'O valiant cousin! worthy gentleman!'
+    );
+  });
+  it('wraps text with line breaks inserted at just the right places to make sure that no line is longer than the column number', () => {
+    expect(wrap('a b a c a b a', 1)).to.equal(
+      'a b a c a b a'.split(' ').join('\n')
+    );
+  });
+});
